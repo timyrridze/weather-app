ea23755876310798d87e24ae8c6ce2a00a250ba1
timyrridze <timursalimov888@gmail.com>
2025-09-27 21:43:59 +0500

    rainAnimation split in splashAnimation and fallAnimation

--- src/animations/rainAnimation/rainAnimation.ts
+++ src/animations/rainAnimation/rainAnimation.ts
@@ -1,33 +1,51 @@
 import "./rainAnimation.css"
 
+
 function getYTranslateCorrection() {
   return navigator.userAgent.includes("Firefox") ? 0.5 : -1
 }
 
-export function rainAnimation(rain: SVGElement, fallCoord: number, svgWidth: number) {
+function fallAnimation<T extends SVGElement>(raindrop: T, fallCoord: number, svgWidth: number) {
+  const raindropCoord = raindrop.getBoundingClientRect().y
+  const raindropHeight = raindrop.getBoundingClientRect().height
+
+  raindrop.style.setProperty('--translate-y', `${(fallCoord - (raindropCoord + raindropHeight)) / (svgWidth * 0.01647207059) + getYTranslateCorrection()}px`)
+
+  raindrop.classList.add("animation-fall")
+}
+
+function splashAnimation<T extends SVGElement>(raindropParts: T) {
+
+}
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export function rainAnimation<T extends SVGElement>(rain: T, fallCoord: number, svgWidth: number) {
 
   return async (resolve: (value: void) => void) => {
     let lastRaindrop = null
 
     for (let i = rain.children.length - 1; i >= 0; i--) {
-      const raindrop = rain.children[i].children[0] as typeof rain
+      const raindrop = rain.children[i].children[0]
+      const raindropParts = rain.children[i].children[1]
+
+      if (!(raindrop instanceof SVGPathElement)) {
+        throw("raindrop expected to be of type SVGPathElement")
+      }
 
       if(!raindrop.classList.contains("raindrop")) {
         throw("element with 'raindrop' class expected as a raindrop")
       }
 
-      await new Promise((resolve: (value: void) => void) => {
-        const raindropCoord = raindrop.getBoundingClientRect().y
-        const raindropHeight = raindrop.getBoundingClientRect().height
+      raindrop.onanimationend = () => {
+        splashAnimation(raindropParts)
+      }
 
-        raindrop.style.setProperty('--translate-y', `${(fallCoord - (raindropCoord + raindropHeight)) / (svgWidth * 0.01647207059) + getYTranslateCorrection()}px`)
+      fallAnimation(raindrop, fallCoord, svgWidth) 
 
-        raindrop.classList.add("animation-fall")
-
-        // console.log(raindrop.getTotalLength())
-
-        setTimeout(() => resolve(), 200)
-      })
+      await sleep(200)
 
       lastRaindrop = raindrop
     }
